c53e3634ced0a4e63d327fce0f4b622ca33891d0
José Santos <jose.dos.santos@outsystems.com>
2021-12-10 09:49:46 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func scan(path string) {
+	print("scan")
+	fmt.Printf("Found folders")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+}
+
+func stats(email string) {
+	print("stats")
+}
+
+func main() {
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
+}
